73ed4e3057f71751426deffce48577c12535a57b
Darryl Dixon <piecedigitalstudios@gmail.com>
2020-02-10 00:05:31 -0500

    adding frame data

---
+++ src/frame_data.hpp
@@ -0,0 +1,173 @@
+#ifndef FRAME_DATA_H
+#define FRAME_DATA_H
+
+#include "deps/nlohmann/json.hpp"
+
+using JSON = nlohmann::json;
+
+struct frameWindow
+{
+    int firstFrameOfWindow;
+    int lastFrameOfWindow;
+};
+
+enum KnockdownType {
+    NORMAL = 0,
+    HARD = 1 << 0,
+    RESET = 1 << 1,
+};
+
+struct KnockdownTypeWindow : frameWindow
+{
+    KnockdownType knockdownType = KnockdownType::NORMAL;
+};
+
+struct KnockupKnockbackWindow : frameWindow
+{
+    int knockupValue;
+    int knockbackValue;
+};
+
+struct LaunchWindow : frameWindow
+{
+    int launchValue;
+};
+
+struct PushbackWindow : frameWindow
+{
+    int pushbackOnHit;
+    int pushbackOnBlock;
+};
+
+struct DamageWindow : frameWindow
+{
+    int damageOnHit;
+    int damageOnBlock;
+};
+
+struct StunWindow : frameWindow
+{
+    int stunValue;
+};
+
+struct CancellableWindow : frameWindow
+{
+    FrameData *cancelMoves;
+};
+
+struct InvincibilityWindow : frameWindow
+{
+    bool hitInvincibility;
+    bool grabInvincibility;
+    bool exHitInvincibility;
+    bool exGrabInvincibility;
+    bool superHitInvincibility;
+    bool superGrabInvincibility;
+};
+
+struct CounterHitStateWindow : frameWindow
+{
+    bool isCounterHitable = false;
+};
+
+struct OverheadWindow : frameWindow
+{
+    bool isOverhead = false;
+};
+
+struct CounterHitOverrideWindow : frameWindow
+{
+    float damageMultiplier = 1.2f;
+    float stunMultiplier = 1.2f;
+    float hitStunMultiplier = 1.2f;
+    float launchMultiplier = 1.2f;
+    float pushbackMultiplier = 1.2f;
+    FrameData *counterHitOnlyCancellables;
+    float Multiplier = 1.2f;
+    KnockdownType knockdownTypeOverride = KnockdownType::HARD;
+};
+
+struct PropulsionWindow : frameWindow
+{
+    int hSpeed;
+    int vSpeed;
+};
+
+struct FrameBoxData
+{
+    int xOffset;
+    int yOffset;
+    int width;
+    int height;
+};
+
+class FrameData
+{
+public:
+    KnockdownTypeWindow *knockdownTypeWindow;
+    int knockdownTypeWindowSize;
+    KnockupKnockbackWindow *knockupKnockbackWindow;
+    int knockupKnockbackWindowSize;
+    LaunchWindow *launchWindow;
+    int launchWindowSize;
+    PushbackWindow *pushbackWindow;
+    int pushbackWindowSize;
+    DamageWindow *damageWindow;
+    int damageWindowSize;
+    StunWindow *stunWindow;
+    int stunWindowSize;
+    CancellableWindow *cancellableWindow;
+    int cancellableWindowSize;
+    InvincibilityWindow *invincibilityWindow;
+    int invincibilityWindowSize;
+    CounterHitStateWindow *counterHitStateWindow;
+    int counterHitStateWindowSize;
+    CounterHitOverrideWindow *counterHitOverrideWindow;
+    int counterHitOverrideWindowSize;
+    OverheadWindow *overheadWindow;
+    int overheadWindowSize;
+    PropulsionWindow *propulsionWindow;
+    int propulsionWindowSize;
+
+    FrameBoxData *frameBoxData;
+    int frameBoxDataSize;
+
+    void frameDataFromJSON(JSON json)
+    {
+        // knockdownTypeWindow
+        // knockupKnockbackWindow
+        // launchWindow
+        // pushbackWindow
+        // damageWindow
+        // stunWindow
+        // cancellableWindow
+        // invincibilityWindow
+        // counterHitStateWindow
+        // counterHitOverrideWindow
+        // overheadWindow
+        // propulsionWindow
+        if (json.contains("frameBoxData"))
+        {
+            if (frameBoxData != NULL)
+            {
+                delete frameBoxData;
+            }
+
+            frameBoxDataSize = json["frameBoxData"].size();
+
+            frameBoxData = new FrameBoxData[frameBoxDataSize];
+
+            for (int i = 0; i < frameBoxDataSize; i++)
+            {
+                frameBoxData[i] = {
+                    json["frameBoxData"]["xOffset"].get<int>(),
+                    json["frameBoxData"]["yOffset"].get<int>(),
+                    json["frameBoxData"]["width"].get<int>(),
+                    json["frameBoxData"]["height"].get<int>()
+                };
+            }
+        }
+    }
+};
+
+#endif
--- src/json_handler.hpp
+++ src/json_handler.hpp
@@ -7,37 +7,8 @@
 
 using JSON = nlohmann::json;
 
-#define OPEN_BRACE "{"
-#define CLOSE_BRACE "}"
-#define OPEN_SQUARE "["
-#define CLOSE_SQUARE "]"
-#define QUOTE "\""
-
 namespace JsonHandler
 {
-    // if I make my own parser I'll come back to this
-    /*
-    enum JsonType
-    {
-        UNKNOWN = 0,
-        // char*
-        STRING,
-        // double
-        NUMBER,
-        // JsonType
-        OBJECT,
-        // JsonType*
-        ARRAY
-    };
-
-    struct JsonRep
-    {
-        JsonType type;
-        bool isSet;
-        void *value;
-    };
-    */
-
     static JSON parseJsonFile(const char *data)
     {
         return JSON::parse(data);
